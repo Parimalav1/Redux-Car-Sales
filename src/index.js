ad0ed0e1f652092910546db4a17e7a1b23d67294
Parimala Vemula <parimala.thodupunoori@gmail.com>
2020-07-15 00:19:27 -0700

    Project-on-Redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// importing createStore, carReducer, provider
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { reducer } from './reducers/carReducer';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+import App from './App';
+
+const store = createStore(reducer);
+console.log("This is my state:", store.getState());
+
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    // wrapping and passing props
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    rootElement);
